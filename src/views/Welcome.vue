75ce80f86d23e7040340295769dba9e734742525
Victor Villazón <vvillazon@itereo.travel>
2021-05-03 23:56:01 -0500

    UI: Update Welcome view

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,8 +1,11 @@
 <template>
-  <div>
-    <h1>Welcome</h1>
-    <router-link to="/loading">Get started</router-link>
-  </div>
+  <section class="welcome text-center">
+    <div class="">
+      <img src="../assets/pikachu.svg" alt="Pikachu" />
+      <h1 class="text-gray-600">Welcome to Pokédex</h1>
+      <p class="text-gray-500">The digital encyclopedia created by Professor Oak is an invaluable tool to Trainers in the Pokémon world.</p>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -10,3 +13,19 @@ export default {
   name: 'WelcomeView'
 }
 </script>
+
+<style lang="scss" scoped>
+  .welcome img {
+    margin-top: 70px;
+    margin-bottom: 60px;
+  }
+
+  .welcome h1 {
+    margin-bottom: 30px;
+  }
+
+  .welcome p {
+    padding: 0 30px;
+    margin-bottom: 50px;
+  }
+</style>
