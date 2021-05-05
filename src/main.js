a32b0cecb2c6226df46d302210855e7eb2141c34
Victor Villazón <victorzn23@gmail.com>
2021-05-05 15:20:18 -0500

    ADD: vue-simple-portal, vue-clipboard2 y vue-lazyload

--- src/main.js
+++ src/main.js
@@ -3,9 +3,20 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import VueClipboard from 'vue-clipboard2'
+import VueLazyload from 'vue-lazyload'
+
 import './components/base-ui/_globals'
+import './components/icons/_globals'
 
 Vue.config.productionTip = false
+
+Vue.use(VueClipboard)
+Vue.use(VueLazyload, {
+  preLoad: 1.3,
+  error: 'images/pokeball.svg',
+  attempt: 1
+})
 
 new Vue({
   router,
