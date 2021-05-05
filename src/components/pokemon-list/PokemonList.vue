d8dda4b1976d676be3c71075f7144f42514387be
Victor Villazón <victorzn23@gmail.com>
2021-05-05 15:22:59 -0500

    UI: Add Back to top button logic in the list bottom

--- src/components/pokemon-list/PokemonList.vue
+++ src/components/pokemon-list/PokemonList.vue
@@ -5,11 +5,18 @@
       :key="index"
       :data="item"
     />
+    <div class="TheEnd text-center" v-show="showButtonBackTop">
+      <h2>The end.</h2>
+      <BaseButton
+        class="btn btn-normal"
+        @click="backToTop">
+        Back to top
+      </BaseButton>
+    </div>
   </div>
 </template>
 
 <script>
-
 import PokemonListItem from '@/components/pokemon-list/PokemonListItem.vue'
 
 export default {
@@ -18,6 +25,15 @@ export default {
   props: {
     data: {
       type: Array
+    },
+    showButtonBackTop: {
+      type: Boolean,
+      default: true
+    }
+  },
+  methods: {
+    backToTop () {
+      window.scrollTo(0, 0)
     }
   }
 }
@@ -26,5 +42,20 @@ export default {
 <style lang="scss" scoped>
   .wrapper-list {
     padding: 0 30px;
+  }
+
+  .TheEnd {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  // Small devices (landscape phones, 576px and up)
+  @media (min-width: 576px) {
+    .wrapper-list {
+      margin: 0 auto;
+      padding: 0 0;
+      width: 570px;
+    }
   }
 </style>
