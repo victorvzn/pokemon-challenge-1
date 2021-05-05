30270c10d5cac906b3dfadb28a9accbe7dde2682
Victor Villazón <vvillazon@itereo.travel>
2021-05-05 06:29:59 -0500

    refactor: Add loader page

---
+++ src/components/pokemon-list/PokemonIsLoading.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="PokemonIsLoading">
+    <IconPokeball />
+  </div>
+</template>
+
+<script>
+import IconPokeball from '@/components/icons/IconPokeball.vue'
+
+export default {
+  name: 'PokemonIsLoading',
+  components: { IconPokeball }
+}
+</script>
+
+<style lang="scss" scoped>
+  .PokemonIsLoading {
+    width: 100vw;
+    height: 50vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
--- src/store/pokemons/actions.js
+++ src/store/pokemons/actions.js
@@ -1,12 +1,17 @@
 import pokemons from '@/services/pokemons'
 
+import { sleeper } from '../helpers'
+
 export default {
   getPokemons ({ commit }, payload) {
+    commit('SET_ISLOADING', true)
     return pokemons.fetchAll(payload)
+      .then(sleeper(500))
       .then(response => {
         if (response && response.status === 200) {
           commit('SET_POKEMON_DATA', response.data)
         }
+        commit('SET_ISLOADING', false)
       })
   }
 }
