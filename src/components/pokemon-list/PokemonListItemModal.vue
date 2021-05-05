205696ecd6e0664b7e43523976fd423a43208ebb
Victor Villazón <victorzn23@gmail.com>
2021-05-05 15:24:32 -0500

    UI: Enhance Modal separating in more components

--- src/components/pokemon-list/PokemonListItemModal.vue
+++ src/components/pokemon-list/PokemonListItemModal.vue
@@ -1,9 +1,88 @@
 <template>
-  <BaseModal />
+  <BaseModal @onClose="onClose">
+    <template v-slot:closeButton>
+      <IconTimes />
+    </template>
+    <div class="PokemonListItemModal">
+      <section class="PokemonListItemModal-bg">
+        <IconPokeball v-if="isLoadingDetail" class="spinner" />
+        <img
+          v-if="!isLoadingDetail"
+          width="180"
+          height="180"
+          loading="lazy"
+          :alt="pokemonSelected.name"
+          v-lazy="pokemonSelected.image" />
+      </section>
+
+      <PokemonListItemModalContent
+        v-if="!isLoadingDetail"
+        :data="pokemonSelected" />
+
+      <section class="PokemonListItemModal-actions"
+        v-if="!isLoadingDetail">
+        <PokemonButtonShare />
+        <PokemonButtonFavorite :data="data" />
+      </section>
+    </div>
+  </BaseModal>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
+import PokemonListItemModalContent from '@/components/pokemon-list/PokemonListItemModalContent.vue'
+import PokemonButtonShare from '@/components/shared/PokemonButtonShare.vue'
+import PokemonButtonFavorite from '@/components/shared/PokemonButtonFavorite.vue'
+
 export default {
-  name: 'PokemonListItemModal'
+  name: 'PokemonListItemModal',
+  components: {
+    PokemonListItemModalContent,
+    PokemonButtonShare,
+    PokemonButtonFavorite
+  },
+  props: {
+    data: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    ...mapGetters({
+      isLoadingDetail: 'pokemons/isLoadingDetail',
+      pokemonSelected: 'pokemons/getPokemon'
+    })
+  },
+  methods: {
+    onClose () {
+      this.$emit('onClose')
+    }
+  }
 }
 </script>
+
+<style scoped>
+  .PokemonListItemModal-bg {
+    background-image: url(/images/bg.svg);
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center center;
+    width: 100%;
+    height: 220px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .PokemonListItemModal-bg img {
+    width: 180px;
+    height: 180px;
+  }
+  .PokemonListItemModal-actions {
+    margin: 20px 30px;
+  }
+  .PokemonListItemModal-actions {
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
---
+++ src/components/pokemon-list/PokemonListItemModalContent.vue
@@ -0,0 +1,48 @@
+<template>
+  <section class="PokemonListItemModal-detail">
+    <div class="PokemonListItemModal-detail-item">
+      <strong>Name:</strong> <span class="capitalize">{{ data.name }}</span>
+    </div>
+    <div class="PokemonListItemModal-detail-item">
+      <strong>Weight:</strong> <span>{{ data.weight }}</span>
+    </div>
+    <div class="PokemonListItemModal-detail-item">
+      <strong>Height:</strong> <span>{{ data.height }}</span>
+    </div>
+    <div class="PokemonListItemModal-detail-item">
+      <strong>Types:</strong> <span class="capitalize">{{ data.types }}</span>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'PokemonListItemModalContent',
+  props: {
+    data: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .PokemonListItemModal-detail-item {
+    padding: 10px 0;
+    border-bottom: 1px solid var(--gray-300);
+  }
+  .PokemonListItemModal-detail-item {
+    font-family: inherit;
+    font-size: 1.125rem; /* 18px / 16px */
+  }
+  .PokemonListItemModal-detail-item strong {
+    font-weight: 700;
+  }
+  .PokemonListItemModal-detail-item span {
+    font-weight: 500;
+  }
+  .PokemonListItemModal-detail {
+    margin: 20px 30px;
+  }
+</style>
