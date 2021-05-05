1f57305f53d57feee261005421471d251f65bd4b
Victor Villazón <vvillazon@itereo.travel>
2021-05-05 06:33:53 -0500

    UI: Add PokemonListFavorites component

--- src/views/PokemonListFavorites.vue
+++ src/views/PokemonListFavorites.vue
@@ -1,12 +1,76 @@
 <template>
-  <div>
-    <h1>Pokemon List Favorittes</h1>
-    <router-link to="/">Home</router-link>
+  <div class="wrapper-list-all">
+    <PokemonIsLoading
+      v-if="isLoading" />
+
+    <template v-if="!isLoading">
+      <PokemonListSearch
+        @onSearch="search" />
+    </template>
+
+    <PokemonMessage
+      v-if="!isLoading && !hasPokemonsFiltered"
+      title="Uh-oh!"
+      description="You don't have favorites yet!"
+      buttonDescription="Go back pokemon list"
+      buttonRouteName="pokemon-list-all" />
+
+    <PokemonList
+      v-if="!isLoading || pokemonsFiltered"
+      :data="pokemonsFiltered" />
+
+    <PokemonListBottomBar v-if="!isLoading" />
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
+import PokemonIsLoading from '@/components/pokemon-list/PokemonIsLoading.vue'
+import PokemonMessage from '@/components/messages/PokemonMessage.vue'
+import PokemonListSearch from '@/components/pokemon-list/PokemonListSearch.vue'
+import PokemonList from '@/components/pokemon-list/PokemonList.vue'
+import PokemonListBottomBar from '@/components/pokemon-list/PokemonListBottomBar.vue'
+
 export default {
-  name: 'PokemonListFavoritesView'
+  name: 'PokemonListFavoritesView',
+  data () {
+    return {
+      keyword: ''
+    }
+  },
+  components: {
+    PokemonIsLoading,
+    PokemonMessage,
+    PokemonListSearch,
+    PokemonList,
+    PokemonListBottomBar
+  },
+  computed: {
+    ...mapGetters({
+      isLoading: 'pokemons/isLoading',
+      getPokemons: 'pokemons/getPokemonsFavorites'
+    }),
+    pokemonsFiltered () {
+      return this.getPokemons(this.keyword)
+    },
+    hasPokemonsFiltered () {
+      if (this.pokemonsFiltered) {
+        return this.pokemonsFiltered.length > 0
+      }
+      return false
+    }
+  },
+  methods: {
+    search (keyword) {
+      this.keyword = keyword
+    }
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+  .wrapper-list-all {
+    margin-top: 130px;
+  }
+</style>
